4ce5a36d0871553897fc49bb57e42cf19bcbf5b0
Bobby Rullo <bobby.rullo@coreos.com>
2015-08-20 12:38:15 -0700

    db: initialize bigint, boolean columns
    gorp hates nil.

--- db/migrations/0002_dex_admin.sql
+++ db/migrations/0002_dex_admin.sql
@@ -1,2 +1,4 @@
 -- +migrate Up
 ALTER TABLE client_identity ADD COLUMN "dex_admin" boolean;
+
+UPDATE "client_identity" SET "dex_admin" = false;
--- functional/repo/testutil.go
+++ functional/repo/testutil.go
@@ -22,6 +22,8 @@ func initDB(dsn string) *gorp.DbMap {
 		panic(fmt.Sprintf("Unable to drop migration table: %v", err))
 	}
 
-	db.MigrateToLatest(c)
+	if _, err = db.MigrateToLatest(c); err != nil {
+		panic(fmt.Sprintf("Unable to migrate: %v", err))
+	}
 	return c
 }
