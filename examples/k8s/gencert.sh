2374345ebbd887a7dbd0e80cb38f037c0545d389
Song.Jin <Song.Jin@myob.com>
2017-06-08 17:13:36 +1000

    update gencert.sh

--- examples/k8s/gencert.sh
+++ examples/k8s/gencert.sh
@@ -18,9 +18,19 @@ subjectAltName = @alt_names
 DNS.1 = dex.example.com
 EOF
 
-openssl genrsa -out ssl/ca-key.pem 2048
+
+# generate ca-key (random)
+# openssl genrsa -out ssl/ca-key.pem 2048
+
+# uses ca-key.pem to generate ca.pem
 openssl req -x509 -new -nodes -key ssl/ca-key.pem -days 10 -out ssl/ca.pem -subj "/CN=kube-ca"
 
+
+# generate key.pem
 openssl genrsa -out ssl/key.pem 2048
+
+# use key.pem AND req.cnf to generate csr.pem (result PART-A)
 openssl req -new -key ssl/key.pem -out ssl/csr.pem -subj "/CN=kube-ca" -config ssl/req.cnf
+
+# uses csr.pem and ca.pem to generate cert.pem (result PART-B)
 openssl x509 -req -in ssl/csr.pem -CA ssl/ca.pem -CAkey ssl/ca-key.pem -CAcreateserial -out ssl/cert.pem -days 10 -extensions v3_req -extfile ssl/req.cnf
