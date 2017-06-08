651cca3229f3bc597b9356a11c23e12ac78d0be0
Song.Jin <Song.Jin@myob.com>
2017-06-08 22:15:34 +1000

    update gencert.sh

--- examples/k8s/gencert.sh
+++ examples/k8s/gencert.sh
@@ -19,18 +19,18 @@ DNS.1 = dex.example.com
 EOF
 
 
-# generate ca-key (random)
+# generate ca-key (random) (the private key)
 # openssl genrsa -out ssl/ca-key.pem 2048
 
-# uses ca-key.pem to generate ca.pem
-openssl req -x509 -new -nodes -key ssl/ca-key.pem -days 10 -out ssl/ca.pem -subj "/CN=kube-ca"
+# uses ca-key.pem to generate ca.pem (the public key)
+openssl req -x509 -new -nodes -key $1 -days 10 -out ssl/ca.pem -subj "/CN=$3"
 
 
-# generate key.pem
+# generate key.pem (random/symetrical key to establish SSL)
 openssl genrsa -out ssl/key.pem 2048
 
 # use key.pem AND req.cnf to generate csr.pem (result PART-A)
-openssl req -new -key ssl/key.pem -out ssl/csr.pem -subj "/CN=kube-ca" -config ssl/req.cnf
+openssl req -new -key ssl/key.pem -out ssl/csr.pem -subj "/CN=$3" -config ssl/req.cnf
 
-# uses csr.pem and ca.pem to generate cert.pem (result PART-B)
-openssl x509 -req -in ssl/csr.pem -CA ssl/ca.pem -CAkey ssl/ca-key.pem -CAcreateserial -out ssl/cert.pem -days 10 -extensions v3_req -extfile ssl/req.cnf
+# uses csr.pem and ca.pem to generate cert.pem (result PART-B) (SSL cert including public key)
+openssl x509 -req -in ssl/csr.pem -CA ssl/ca.pem -CAkey $1 -CAcreateserial -out ssl/cert.pem -days 10 -extensions v3_req -extfile ssl/req.cnf
