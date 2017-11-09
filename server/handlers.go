0a32cbf8798a0e6f3946dd8e51816c9b7e8da239
Stephan Renatus <srenatus@chef.io>
2017-11-09 14:17:02 +0100

    oauth2: don't return an access_token if token wasn't in response_type
    Stumbling upon this part of the OIDC spec[1],
    
    > No Access Token is returned when the value is id_token.
    
    I've realised that we don't comply. Now, we should.
    
    [1]: https://openid.net/specs/openid-connect-core-1_0.html#ImplicitAuthRequest
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- server/handlers.go
+++ server/handlers.go
@@ -491,6 +491,9 @@ func (s *Server) sendCodeResponse(w http.ResponseWriter, r *http.Request, authRe
 		// the "normal" code flow?
 		implicitOrHybrid = false
 
+		// Was a token requested? (Implicit flow can request ONLY id_token)
+		requestedToken = false
+
 		// Only present in hybrid or code flow. code.ID == "" if this is not set.
 		code storage.AuthCode
 
@@ -532,6 +535,7 @@ func (s *Server) sendCodeResponse(w http.ResponseWriter, r *http.Request, authRe
 			}
 		case responseTypeToken:
 			implicitOrHybrid = true
+			requestedToken = true
 		case responseTypeIDToken:
 			implicitOrHybrid = true
 			var err error
@@ -546,7 +550,9 @@ func (s *Server) sendCodeResponse(w http.ResponseWriter, r *http.Request, authRe
 
 	if implicitOrHybrid {
 		v := url.Values{}
-		v.Set("access_token", accessToken)
+		if requestedToken {
+			v.Set("access_token", accessToken)
+		}
 		v.Set("token_type", "bearer")
 		v.Set("state", authReq.State)
 		if idToken != "" {
