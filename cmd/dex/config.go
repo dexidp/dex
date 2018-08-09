17991209c40d9d9c6c501843518c1e558c538549
Ingo Pansa <ic-consult.pansa@extaccount.com>
2018-08-09 09:08:03 +0200

    reformats, part2

--- cmd/dex/config.go
+++ cmd/dex/config.go
@@ -52,10 +52,10 @@ type password storage.Password
 
 func (p *password) UnmarshalJSON(b []byte) error {
 	var data struct {
-		Email    string `json:"email"`
-		Username string `json:"username"`
-		UserID   string `json:"userID"`
-		Hash     string `json:"hash"`
+		Email    string   `json:"email"`
+		Username string   `json:"username"`
+		UserID   string   `json:"userID"`
+		Hash     string   `json:"hash"`
 		Groups   []string `json:"groups"`
 	}
 	if err := json.Unmarshal(b, &data); err != nil {
@@ -65,7 +65,7 @@ func (p *password) UnmarshalJSON(b []byte) error {
 		Email:    data.Email,
 		Username: data.Username,
 		UserID:   data.UserID,
-		Groups:	  data.Groups,
+		Groups:   data.Groups,
 	})
 	if len(data.Hash) == 0 {
 		return fmt.Errorf("no password hash provided")
