5f0a03a06b3c67540f93fe95a95105aedc5ec3f7
Song.Jin <songjin@hotmail.com>
2018-11-06 11:05:36 +1100

    modify log msg as per suggested

--- storage/kubernetes/storage.go
+++ storage/kubernetes/storage.go
@@ -149,10 +149,12 @@ func (cli *client) registerCustomResources(useTPR bool) (ok bool) {
 		} else {
 			r := customResourceDefinitions[i]
 			var i interface{}
-			cli.logger.Infof("checking if resource %s has been created already...", r.ObjectMeta.Name)
+			cli.logger.Infof("checking if custom resource %s has been created already...", r.ObjectMeta.Name)
 			if err := cli.list(r.Spec.Names.Plural, &i); err == nil {
-				cli.logger.Infof("The custom resource already created %s, hence continue", r.ObjectMeta.Name)
+				cli.logger.Infof("The custom resource %s already available, skipping create", r.ObjectMeta.Name)
 				continue
+			} else {
+				cli.logger.Infof("failed to list custom resource %s, attempting to create: %v", r.ObjectMeta.Name, err)
 			}
 			err = cli.postResource("apiextensions.k8s.io/v1beta1", "", "customresourcedefinitions", r)
 			resourceName = r.ObjectMeta.Name
