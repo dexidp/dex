67775ce25de2fc11ba2b6130584bfaef1b203581
Richard Lindhout <richardlindhout96@gmail.com>
2019-06-26 13:18:00 +0200

    Revert back to original

--- server/server.go
+++ server/server.go
@@ -253,10 +253,10 @@ func newServer(ctx context.Context, c Config, rotationStrategy rotationStrategy)
 	}
 	handleWithCORS := func(p string, h http.HandlerFunc) {
 		var handler http.Handler = h
-		// if len(c.AllowedOrigins) > 0 {
-		corsOption := handlers.AllowedOrigins(c.AllowedOrigins)
-		handler = handlers.CORS(corsOption)(handler)
-		// }
+		if len(c.AllowedOrigins) > 0 {
+			corsOption := handlers.AllowedOrigins(c.AllowedOrigins)
+			handler = handlers.CORS(corsOption)(handler)
+		}
 		r.Handle(path.Join(issuerURL.Path, p), instrumentHandlerCounter(p, handler))
 	}
 	r.NotFoundHandler = http.HandlerFunc(http.NotFound)
