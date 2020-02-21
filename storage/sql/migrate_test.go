a53a720d92d024dab003f55265761f70afee2842
Nandor Kracser <bonifaido@gmail.com>
2020-02-21 12:13:38 +0100

    storage/sql: allow specifying sql flavor specific migrations
    Signed-off-by: Nandor Kracser <bonifaido@gmail.com>

--- storage/sql/migrate_test.go
+++ storage/sql/migrate_test.go
@@ -30,8 +30,15 @@ func TestMigrate(t *testing.T) {
 		return sqlErr.ExtendedCode == sqlite3.ErrConstraintUnique
 	}
 
-	c := &conn{db, flavorSQLite3, logger, errCheck}
-	for _, want := range []int{len(migrations), 0} {
+	var sqliteMigrations []migration
+	for _, m := range migrations {
+		if m.flavor == nil || m.flavor == &flavorSQLite3 {
+			sqliteMigrations = append(sqliteMigrations, m)
+		}
+	}
+
+	c := &conn{db, &flavorSQLite3, logger, errCheck}
+	for _, want := range []int{len(sqliteMigrations), 0} {
 		got, err := c.migrate()
 		if err != nil {
 			t.Fatal(err)
