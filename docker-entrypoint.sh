7f744598f5232ba4c5f1c458d7911785ca67311d
m.nabokikh <maksim.nabokikh@flant.com>
2021-01-30 14:54:17 +0400

    Add detailed description to docker-entrypoint.sh
    Signed-off-by: m.nabokikh <maksim.nabokikh@flant.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -e
 
 ### Usage: /docker-entrypoint.sh <command> <args>
+### * If command equals to "serve", config file for serving will be preprocessed using gomplate and saved to tmp dir.
+###   Example: docker-entrypoint.sh serve config.yaml = dex serve /tmp/dex-config.yaml-ABCDEFG
+### * If command is not in the list of known dex commands, it will be executed bypassing entrypoint.
+###   Example: docker-entrypoint.sh echo "Hello!" = echo "Hello!"
+
 command=$1
 
 case "$command" in
