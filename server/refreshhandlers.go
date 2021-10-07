ae56694aaca24643d8d2d13269a20be63e9b78e5
pipo02mix <fernando@giantswarm.io>
2021-10-07 11:20:30 +0200

    Add custom oidc group handling

--- server/refreshhandlers.go
+++ server/refreshhandlers.go
@@ -190,6 +190,19 @@ func (s *Server) refreshWithConnector(ctx context.Context, token *internal.Refre
 		ident = newIdent
 	}
 
+	{
+		var oidcGroups []string
+		if s.oidcGroupsPrefix {
+			prefix := refresh.ConnectorID
+
+			for _, group := range ident.Groups {
+				prefixedGroup := fmt.Sprintf("%s:%s", prefix, group)
+				oidcGroups = append(oidcGroups, prefixedGroup)
+			}
+			ident.Groups = oidcGroups
+		}
+	}
+
 	return ident, nil
 }
 
