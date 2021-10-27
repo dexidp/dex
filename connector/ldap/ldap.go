d74002f9c182ec997a361233e8c50961ef1245ca
Jeffrey Frey <freyguy77@gmail.com>
2021-10-27 13:51:38 -0400

    Check for mandatory emailAttr or emailSuffix userSearch option

--- connector/ldap/ldap.go
+++ connector/ldap/ldap.go
@@ -284,6 +284,10 @@ func (c *Config) openConnector(logger log.Logger) (*ldapConnector, error) {
 		}
 	}
 
+	if c.UserSearch.EmailAttr == "" && c.UserSearch.EmailSuffix == "" {
+		return nil, fmt.Errorf("ldap: either emailAttr or emailSuffix must be provided")
+	}
+
 	var (
 		host string
 		err  error
