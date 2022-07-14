2d70863e98f1c88faeca0db79d087a6afb12a40b
Miguel <miguel.mc@ironchip.net>
2022-07-14 15:38:27 +0200

    mongo example added with docker-compose.yml
    Signed-off-by: Miguel <miguel.mc@ironchip.net>

---
+++ examples/mongo/README.md
@@ -0,0 +1,15 @@
+# MONGODB Storage
+
+## Requirements
+    * MongoDB replicaset
+
+## Develop
+Run:
+```
+./start-db.sh
+```
+Test:
+```
+DEX_MONGO_URI=mongodb://localhost:27021/oidc?directConnection=true
+```
+
---
+++ examples/mongo/rs-init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mongo <<EOF
+var config = {
+    "_id": "dbrs",
+    "version": 1,
+    "members": [
+        {
+            "_id": 1,
+            "host": "mongo1:27017",
+            "priority": 3
+        },
+        {
+            "_id": 2,
+            "host": "mongo2:27017",
+            "priority": 2
+        },
+        {
+            "_id": 3,
+            "host": "mongo3:27017",
+            "priority": 1
+        }
+    ]
+};
+rs.initiate(config, { force: true });
+rs.status();
+EOF
