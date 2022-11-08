2849a6c70f27e2c76d01a3d685db8cd92fc30b01
Dylan Moreland <dylan@dimo.zone>
2022-11-08 10:47:48 -0500

    BE-53 Better challenge message (#7)
    * Message now explains the exchange and mentions the redirect hostname and Ethereum address
    * Upgrade nonce to system randomness and make it wider

--- server/handlers.go
+++ server/handlers.go
@@ -2,13 +2,14 @@ package server
 
 import (
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha256"
 	"crypto/subtle"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"math/rand"
+	"math/big"
 	"net/http"
 	"net/url"
 	"path"
@@ -215,8 +216,20 @@ func (s *Server) handleChallenge(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	nonce := generateNonce()
-	bts, err := json.Marshal(web3ConnectorData{Address: nonceReq.Address, Nonce: nonce})
+	u, err := url.Parse(authReq.RedirectURI)
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusBadRequest, "Invalid redirect URI")
+	}
+
+	nonce, err := generateNonce()
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusInternalServerError, "No source of randomness.")
+		return
+	}
+
+	challenge := fmt.Sprintf("%s is asking you to please verify ownership of the address %s by signing this random string: %s", u.Hostname(), nonceReq.Address, nonce)
+
+	bts, err := json.Marshal(web3ConnectorData{Address: nonceReq.Address, Nonce: challenge})
 	if err != nil {
 		s.renderErrorJSON(w, http.StatusInternalServerError, "Failed to create auth request.")
 		return
@@ -231,17 +244,21 @@ func (s *Server) handleChallenge(w http.ResponseWriter, r *http.Request) {
 
 	s.renderJSON(w, struct {
 		Nonce string `json:"nonce"`
-	}{nonce})
+	}{challenge})
 }
 
-func generateNonce() string {
-	const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789"
-
-	b := make([]byte, 8)
+func generateNonce() (string, error) {
+	const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	alphabetSize := big.NewInt(int64(len(alphabet)))
+	b := make([]byte, 30)
 	for i := range b {
-		b[i] = allowedChars[rand.Intn(len(allowedChars))]
+		c, err := rand.Int(rand.Reader, alphabetSize)
+		if err != nil {
+			return "", err
+		}
+		b[i] = alphabet[c.Int64()]
 	}
-	return string(b)
+	return string(b), nil
 }
 
 func (s *Server) handleVerify(w http.ResponseWriter, r *http.Request) {
