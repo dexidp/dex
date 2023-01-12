ff17a3ea6827dd600d4674d2ec33dbb07fdbea98
Doyin.O <111298305+0xdev22@users.noreply.github.com>
2023-01-12 16:46:45 -0500

    Update server/api.go
    Co-authored-by: Dylan Moreland <79415431+elffjs@users.noreply.github.com>

--- server/api.go
+++ server/api.go
@@ -405,7 +405,7 @@ func (d dexAPI) GetCustomToken(ctx context.Context, req *api.SignTokenRequest) (
 
 	token, err := CreateCustomToken(d.s, d.logger, payload)
 	if err != nil {
-		return nil, fmt.Errorf("failed to generate Jwt token: %v", err)
+		return nil, fmt.Errorf("failed to generate JWT: %v", err)
 	}
 
 	return &api.SignTokenResp{
