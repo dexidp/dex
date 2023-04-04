8838917198d6bd7cbc75fbdf0306fa5147342625
Dylan Moreland <dylan@dimo.zone>
2023-04-04 16:57:35 -0400

    Two-step endpoints

--- server/handlers.go
+++ server/handlers.go
@@ -197,6 +197,89 @@ type web3ConnectorData struct {
 
 var addressRegex = regexp.MustCompile("^0x[a-fA-F0-9]{40}$")
 
+// Create an authorization request and nonce for a web3 login. Return state (the request id)
+// and a random nonce.
+func (s *Server) handleGenerateChallenge(w http.ResponseWriter, r *http.Request) {
+	authReq, err := s.parseAuthorizationRequest(r)
+	if err != nil {
+		s.logger.Errorf("Failed to parse authorization request: %v", err)
+
+		switch authErr := err.(type) {
+		case *redirectedAuthErr:
+			authErr.Handler().ServeHTTP(w, r)
+		case *displayedAuthErr:
+			s.renderError(r, w, authErr.Status, err.Error())
+		default:
+			panic("unsupported error type")
+		}
+
+		return
+	}
+
+	u, err := url.Parse(authReq.RedirectURI)
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusBadRequest, "Invalid redirect URI.")
+		return
+	}
+
+	var gcReq struct {
+		Address common.Address `json:"address"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&gcReq); err != nil {
+		s.renderErrorJSON(w, http.StatusBadRequest, "Could not parse request body.")
+		return
+	}
+
+	connID := mux.Vars(r)["connector"]
+	_, err = s.getConnector(connID)
+	if err != nil {
+		s.logger.Errorf("Failed to get connector: %v", err)
+		s.renderError(r, w, http.StatusBadRequest, "Could not retrieve connector.")
+		return
+	}
+
+	// Set the connector being used for the login.
+	if authReq.ConnectorID != "" && authReq.ConnectorID != connID {
+		s.logger.Errorf("Mismatched connector ID in auth request: %s vs %s",
+			authReq.ConnectorID, connID)
+		s.renderError(r, w, http.StatusBadRequest, "Bad connector ID")
+		return
+	}
+
+	authReq.ConnectorID = connID
+
+	nonce, err := generateNonce()
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusInternalServerError, "No source of randomness.")
+		return
+	}
+
+	challenge := fmt.Sprintf("%s is asking you to please verify ownership of the address %s by signing this random string: %s", u.Hostname(), gcReq.Address.Hex(), nonce)
+
+	authReq.ConnectorData, err = json.Marshal(web3ConnectorData{Address: gcReq.Address.Hex(), Nonce: challenge})
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusInternalServerError, "Failed to create auth request.")
+		return
+	}
+
+	// Actually create the auth request
+	authReq.Expiry = s.now().Add(s.authRequestsValidFor)
+	if err := s.storage.CreateAuthRequest(*authReq); err != nil {
+		s.logger.Errorf("Failed to create authorization request: %v", err)
+		s.renderError(r, w, http.StatusInternalServerError, "Failed to connect to the database.")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	s.renderJSON(w, struct {
+		State string `json:"state"`
+		Nonce string `json:"nonce"`
+	}{authReq.ID, challenge})
+}
+
 func (s *Server) handleChallenge(w http.ResponseWriter, r *http.Request) {
 	var nonceReq struct {
 		Address string `json:"address"`
@@ -401,6 +484,93 @@ func (s *Server) handleVerifyDirect(w http.ResponseWriter, r *http.Request) {
 	s.renderJSON(w, struct {
 		Redirect string `json:"code"`
 	}{code.ID})
+}
+
+// Handle the usual token request, except instead of the code we look for
+// state (the auth request) and the sugnature.
+func (s *Server) handleSubmitChallenge(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		s.renderErrorJSON(w, http.StatusBadRequest, "Couldn't parse form.")
+		return
+	}
+
+	authReqID := r.PostFormValue("state")
+	authReq, err := s.storage.GetAuthRequest(authReqID)
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusBadRequest, "Requested resource does not exist.")
+		return
+	}
+
+	var data web3ConnectorData
+	json.Unmarshal(authReq.ConnectorData, &data)
+
+	conn, err := s.getConnector(authReq.ConnectorID)
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusInternalServerError, "Requested resource does not exist.")
+		return
+	}
+
+	w3Conn, ok := conn.Connector.(connector.Web3Connector)
+	if !ok {
+		s.renderErrorJSON(w, http.StatusInternalServerError, "Requested resource does not exist.")
+		return
+	}
+
+	identity, err := w3Conn.Verify(data.Address, data.Nonce, r.PostFormValue("signed"))
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusBadRequest, "Could not verify signature.")
+		return
+	}
+
+	_, _, err = s.finalizeLogin(identity, authReq, conn)
+	if err != nil {
+		s.renderErrorJSON(w, http.StatusInternalServerError, "Login failure.")
+	}
+
+	// Need to pick up the changes made by finalizeLogin. This is pretty gross!
+	authReq, err = s.storage.GetAuthRequest(authReqID)
+	if err != nil {
+		s.logger.Errorf("Failed to get auth request: %v", err)
+		s.renderError(r, w, http.StatusInternalServerError, "Database error.")
+		return
+	}
+
+	if s.now().After(authReq.Expiry) {
+		s.renderErrorJSON(w, http.StatusBadRequest, "User session has expired.")
+		return
+	}
+
+	if err := s.storage.DeleteAuthRequest(authReq.ID); err != nil {
+		if err != storage.ErrNotFound {
+			s.logger.Errorf("Failed to delete authorization request: %v", err)
+			s.renderErrorJSON(w, http.StatusInternalServerError, "Internal server error.")
+		} else {
+			s.renderErrorJSON(w, http.StatusBadRequest, "User session error.")
+		}
+		return
+	}
+
+	code := storage.AuthCode{
+		ID:            storage.NewID(),
+		ClientID:      authReq.ClientID,
+		ConnectorID:   authReq.ConnectorID,
+		Nonce:         authReq.Nonce,
+		Scopes:        authReq.Scopes,
+		Claims:        authReq.Claims,
+		Expiry:        s.now().Add(time.Minute * 30),
+		RedirectURI:   authReq.RedirectURI,
+		ConnectorData: authReq.ConnectorData,
+		PKCE:          authReq.PKCE,
+	}
+	if err := s.storage.CreateAuthCode(code); err != nil {
+		s.logger.Errorf("Failed to create auth code: %v", err)
+		s.renderError(r, w, http.StatusInternalServerError, "Internal server error.")
+		return
+	}
+
+	r.PostForm.Set("code", code.ID)
+
+	s.handleToken(w, r)
 }
 
 func (s *Server) handleCreateAuthorizationRequest(w http.ResponseWriter, r *http.Request) {
