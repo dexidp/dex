0ceb39aa0789570d0b86d536e487b8e785d4b9d9
Houssem Ben Mabrouk <houssem.benmabrouk.ext@orange.com>
2023-10-19 11:21:02 +0200

    add prefix non-root-path to redirect_uri
    Signed-off-by: Houssem Ben Mabrouk <houssem.benmabrouk.ext@orange.com>

--- server/server_test.go
+++ server/server_test.go
@@ -1630,7 +1630,7 @@ func TestOAuth2DeviceFlow(t *testing.T) {
 				// Add the Clients to the test server
 				client := storage.Client{
 					ID:           clientID,
-					RedirectURIs: []string{deviceCallbackURI},
+					RedirectURIs: []string{"/non-root-path" + deviceCallbackURI},
 					Public:       true,
 				}
 				if err := s.storage.CreateClient(client); err != nil {
