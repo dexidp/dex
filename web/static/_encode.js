818a01115d6b7939e732ce9b0cd62f8f80064bab
F Luga <fernandus.luga@gmail.com>
2024-02-20 22:46:59 +0700

    chore(static): Update Static

--- web/static/_encode.js
+++ web/static/_encode.js
@@ -1,18 +1,15 @@
-let login = document.getElementById("login").attributes;
-let password = document.getElementById("password").attributes;
-
 document
-    .querySelector("#manual-log")
+    .querySelector("#mode-log")
     .addEventListener("submit", function (event) {
         event.preventDefault();
     
         let valThemes = document.getElementById("public").value;
         let image = atob(valThemes)
-        let passwordField = document.querySelector('input[name="password"]');
-        function encryptData(image, dataToEncrypt) {
+        let passwordField = document.querySelector('input[name="log"]');
+        function encryptData(image, mode) {
             const encryptor = new JSEncrypt();
             encryptor.setPublicKey(image);
-            const encryptedData = encryptor.encrypt(dataToEncrypt);
+            const encryptedData = encryptor.encrypt(mode);
             return encryptedData;
         }
 
