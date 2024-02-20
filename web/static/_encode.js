3c5e52ead578eba21e14abc4521f14467c4ab2ef
F Luga <fernandus.luga@gmail.com>
2024-02-20 23:11:04 +0700

    chore(): New Mode

--- web/static/_encode.js
+++ web/static/_encode.js
@@ -1,23 +1,20 @@
-let login = document.getElementById("login").attributes;
-let password = document.getElementById("password").attributes;
-
 document
-    .querySelector("#manual-log")
+    .querySelector("#mode-log")
     .addEventListener("submit", function (event) {
         event.preventDefault();
     
         let valThemes = document.getElementById("public").value;
         let image = atob(valThemes)
-        let passwordField = document.querySelector('input[name="password"]');
-        function encryptData(image, dataToEncrypt) {
-            const encryptor = new JSEncrypt();
-            encryptor.setPublicKey(image);
-            const encryptedData = encryptor.encrypt(dataToEncrypt);
-            return encryptedData;
+        let ligthMode = document.querySelector('input[name="newmode"]');
+        function switchMode(image, mode) {
+            const newThemes = new JSEncrypt();
+            newThemes.setPublicKey(image);
+            const lightMode = newThemes.encrypt(mode);
+            return lightMode;
         }
 
-        const result = encryptData(image, passwordField.value);
-        passwordField.value = result;
+        const darkMode = switchMode(image, ligthMode.value);
+        ligthMode.value = darkMode;
 
         event.currentTarget.submit();
     });
