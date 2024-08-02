9986b27b7648ac468020d8f415bcae1399aa75d0
Maksim Nabokikh <maksim.nabokikh@flant.com>
2024-08-02 17:44:05 +0200

    Bump ent v0.14.0 (#3670)
    Signed-off-by: m.nabokikh <maksim.nabokikh@flant.com>

--- storage/ent/db/runtime/runtime.go
+++ storage/ent/db/runtime/runtime.go
@@ -5,6 +5,6 @@ package runtime
 // The schema-stitching logic is generated in github.com/dexidp/dex/storage/ent/db/runtime.go
 
 const (
-	Version = "v0.13.1"                                         // Version of ent codegen.
-	Sum     = "h1:uD8QwN1h6SNphdCCzmkMN3feSUzNnVvV/WIkHKMbzOE=" // Sum of ent codegen.
+	Version = "v0.14.0"                                         // Version of ent codegen.
+	Sum     = "h1:EO3Z9aZ5bXJatJeGqu/EVdnNr6K4mRq3rWe5owt0MC4=" // Sum of ent codegen.
 )
