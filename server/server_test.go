553d1a9e51fd024d25dc526111575bdb78f54568
Shubham Mali <shubham@platform9.com>
2024-11-27 15:28:03 +0000

    add missing test changes

--- server/server_test.go
+++ server/server_test.go
@@ -1749,7 +1749,7 @@ func TestServerSupportedGrants(t *testing.T) {
 		},
 		{
 			name:      "With password connector",
-			config:    func(c *Config) { c.PasswordConnector = "local" },
+			config:    func(c *Config) { c.DefaultPasswordConnector = "local" },
 			resGrants: []string{grantTypeAuthorizationCode, grantTypePassword, grantTypeRefreshToken, grantTypeDeviceCode},
 		},
 		{
@@ -1760,7 +1760,7 @@ func TestServerSupportedGrants(t *testing.T) {
 		{
 			name: "All",
 			config: func(c *Config) {
-				c.PasswordConnector = "local"
+				c.DefaultPasswordConnector = "local"
 				c.SupportedResponseTypes = append(c.SupportedResponseTypes, responseTypeToken)
 			},
 			resGrants: []string{grantTypeAuthorizationCode, grantTypeImplicit, grantTypePassword, grantTypeRefreshToken, grantTypeDeviceCode},
