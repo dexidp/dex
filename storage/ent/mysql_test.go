99464f8cf391d8c8d8ae40ef9011ab98f02471c1
maksim.nabokikh <max.nabokih@gmail.com>
2026-03-06 08:27:58 +0100

    feat(tests): add MySQL 8 support in CI and tests
    Signed-off-by: maksim.nabokikh <max.nabokih@gmail.com>

--- storage/ent/mysql_test.go
+++ storage/ent/mysql_test.go
@@ -18,6 +18,12 @@ const (
 	MySQLEntDatabaseEnv = "DEX_MYSQL_ENT_DATABASE"
 	MySQLEntUserEnv     = "DEX_MYSQL_ENT_USER"
 	MySQLEntPasswordEnv = "DEX_MYSQL_ENT_PASSWORD"
+
+	MySQL8EntHostEnv     = "DEX_MYSQL8_ENT_HOST"
+	MySQL8EntPortEnv     = "DEX_MYSQL8_ENT_PORT"
+	MySQL8EntDatabaseEnv = "DEX_MYSQL8_ENT_DATABASE"
+	MySQL8EntUserEnv     = "DEX_MYSQL8_ENT_USER"
+	MySQL8EntPasswordEnv = "DEX_MYSQL8_ENT_PASSWORD"
 )
 
 func mysqlTestConfig(host string, port uint64) *MySQL {
@@ -38,6 +44,35 @@ func mysqlTestConfig(host string, port uint64) *MySQL {
 			"innodb_lock_wait_timeout": "1",
 		},
 	}
+}
+
+func mysql8TestConfig(host string, port uint64) *MySQL {
+	return &MySQL{
+		NetworkDB: NetworkDB{
+			Database: getenv(MySQL8EntDatabaseEnv, "mysql"),
+			User:     getenv(MySQL8EntUserEnv, "mysql"),
+			Password: getenv(MySQL8EntPasswordEnv, "mysql"),
+			Host:     host,
+			Port:     uint16(port),
+		},
+		SSL: SSL{
+			Mode: mysqlSSLFalse,
+		},
+		params: map[string]string{
+			"innodb_lock_wait_timeout": "1",
+		},
+	}
+}
+
+func newMySQL8Storage(t *testing.T, host string, port uint64) storage.Storage {
+	logger := slog.New(slog.NewTextHandler(t.Output(), &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	cfg := mysql8TestConfig(host, port)
+	s, err := cfg.Open(logger)
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
 
 func newMySQLStorage(t *testing.T, host string, port uint64) storage.Storage {
@@ -67,6 +102,27 @@ func TestMySQL(t *testing.T) {
 
 	newStorage := func(t *testing.T) storage.Storage {
 		return newMySQLStorage(t, host, port)
+	}
+	conformance.RunTests(t, newStorage)
+	conformance.RunTransactionTests(t, newStorage)
+}
+
+func TestMySQL8(t *testing.T) {
+	host := os.Getenv(MySQL8EntHostEnv)
+	if host == "" {
+		t.Skipf("test environment variable %s not set, skipping", MySQL8EntHostEnv)
+	}
+
+	port := uint64(3306)
+	if rawPort := os.Getenv(MySQL8EntPortEnv); rawPort != "" {
+		var err error
+
+		port, err = strconv.ParseUint(rawPort, 10, 32)
+		require.NoError(t, err, "invalid mysql port %q: %s", rawPort, err)
+	}
+
+	newStorage := func(t *testing.T) storage.Storage {
+		return newMySQL8Storage(t, host, port)
 	}
 	conformance.RunTests(t, newStorage)
 	conformance.RunTransactionTests(t, newStorage)
